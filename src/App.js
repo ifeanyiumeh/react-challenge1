1777306dd1620b65a7cb532e4a95c6d77e9465f7
Ifeanyi Umeh <somto_umeh@yahoo.com>
2022-02-24 18:29:41 -0500

    Completed first challenge in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const user = {
+  name: "Somto's pussy",
+  location: "OTTAWA CITY IS MY TOWN",
+  foodType: "UBER YEETS",
+  age: 23,
+  likes: "Coding & JEerking off into the wee hours of the morning",
+  twitterUsername: "e4anyi",
+  avatar:
+    "https://res.cloudinary.com/betterdev/image/upload/v1624211301/chris_ge6pzx.jpg",
+};
 
 function App() {
+  const url = `https://twitter.com/${user.twitterUsername}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="user-deets">
+      <a href={url}>
+        <img src={user.avatar} alt={user.name} />
+      </a>
+      <a href={url}>
+        <h3>{user.name}</h3>
+      </a>
+
+      <p>
+        <strong>Eats</strong>
+        {user.foodType}
+      </p>
+      <p>
+        <strong>Age</strong>
+        {user.age}
+      </p>
+      <p>
+        <strong>Likes</strong>
+        {user.likes}
+      </p>
+      <p>
+        <strong>Twitter Handle</strong>
+        <a href={url}>@{user.twitterUsername}</a>
+      </p>
+      <p>
+        <strong>location</strong>
+        {user.location}
+      </p>
     </div>
   );
 }
